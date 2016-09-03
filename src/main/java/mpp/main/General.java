4aa56b3e1b9c48db07adda0bdc671388d3957f1a
Demihov Vladimir <djemann13@yandex.ru>
2016-09-04 00:01:23 +0300

    +

---
+++ src/main/java/mpp/main/General.java
@@ -0,0 +1,13 @@
+package mpp.main;
+
+/**
+ * created on 03.09.2016
+ * @author vdemihov
+ * основной класс приложения
+ */
+public class General {
+    public static void main(String args[] )
+    {
+        System.out.println("Test git");
+    }
+}
